d00b94be4509b1f1e0fd7b073228a063a3acbfeb
imhyunjun <limhyunjun04@naver.com>
2022-03-27 13:36:56 +0900

    20220327

--- String/5430 AC.cpp
+++ String/5430 AC.cpp
@@ -1,97 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
-
+#include <algorithm>
+#include <string>
+#include<deque>
 using namespace std;
 
-vector<string> split(string input, char delimiter) {
-	vector<string> answer;
-	stringstream ss(input);
-	string temp;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, t;
 
-	while (getline(ss, temp, delimiter)) {
-		answer.push_back(temp);
-	}
+    cin >> t;
+    while (t--) {
+        string order, str;
+        bool reverse = false, error = false;
+        deque<int> dq;
+        cin >> order;
+        cin >> n;
+        cin >> str;
+        string s = "";
+        for (int i = 0; i < str.length(); i++) {
 
-	return answer;
-}
+            if (isdigit(str[i])) {
+                s += str[i];
 
-int main()
-{
-	int T;
-	cin >> T;
-	while (T > 0)
-	{
-		string p;
-		int n;
-		string a;
-		bool isError = false;
+            }
+            else {
+                if (!s.empty()) {
+                    dq.push_back(stoi(s));
+                    s = "";
+                }
+            }
+        }
+        for (auto o : order) {
+            if (o == 'R') {
+                if (reverse)
+                    reverse = false;
+                else
+                    reverse = true;
+            }
+            else {
+                if (dq.empty()) {
+                    cout << "error" << '\n';
+                    error = true;
+                    break;
+                }
+                if (reverse)
+                    dq.pop_back();
+                else
+                    dq.pop_front();
+            }
+        }
+        if (!error) {
+            cout << '[';
+        }
+        if (reverse && !dq.empty()) {
+            for (auto o = dq.rbegin(); o != dq.rend(); o++) {
+                if (o == dq.rend() - 1)
+                    cout << *o;
+                else
+                    cout << *o << ',';
+            }
+        }
+        else if (!reverse && !dq.empty()) {
+            for (auto o = dq.begin(); o != dq.end(); o++) {
+                if (o == dq.end() - 1)
+                    cout << *o;
+                else
+                    cout << *o << ',';
+            }
+        }
+        if (!error)
+            cout << "]\n";
 
-		cin >> p >> n >> a;
-		string as;
-		vector<string> asv;
-		if (a.size() > 2)
-			as = a.substr(1, a.size() - 2);
-		else
-			as = "";
-		asv = split(as, ',' );
-
-		for (int i = 0; i < p.size(); i++)
-		{
-			char f = p.at(i);
-			if (f == 'R')
-			{
-				int half = n / 2;
-				if (half > 1)
-				{
-					for (int i = 0; i < half; i++)
-					{
-						string temp = asv[i];
-						asv[i] = asv[n - i - 1];
-						asv[n - i - 1] = temp;
-					}
-				}
-			}
-			else if (f == 'D')
-			{
-				if (asv.size() < 1 || asv[0] == "0")
-				{
-					isError = true;
-					cout << "error" << "\n";
-				}
-				else
-				{
-					if(asv.size() >= 1)
-						asv[asv.size() - 1] = "0";
-
-					for (int i = 0; i < n - 1; i++)
-					{
-						asv[i] = asv[i + 1];
-						asv[i + 1] = "0";
-					}
-				}
-			}
-		}
-
-		if (!isError)
-		{
-			cout << "[" << asv[0];
-			for (int i = 1; i < n; i++)
-			{
-				if (asv[i] == "0")
-				{
-					cout << "]" << "\n";
-					break;
-				}
-				else
-				{
-					cout << "," << asv[i];
-				}
-			}
-		}
-
-		T--;
-	}
-
-	return 0;
+    }
 }
